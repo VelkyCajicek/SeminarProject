95ac0c0539daf0323e6664917de3f116c21c26b8
unknown <peichl294@gmail.com>
2024-04-17 11:17:41 +0200

    Added chunks and customizable terrain

--- Assets/Scripts/WorldGeneration.cs
+++ Assets/Scripts/WorldGeneration.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class WorldGeneration : MonoBehaviour
 {
-    public Sprite tile;
-    public int worldSize = 100;
+    
+    public Sprite bottomSand;
+    public Sprite topSand;
+    public Sprite stone;
+    
+    public int sandLayerHeight = 5;
+
+    public int worldSize = 160;
+    public int chunkSize = 16;
+    public GameObject[] worldChunks;
+
     public float noiseFrequency = 0.04f;
     public float seed;
     public float heightMultiplier = 25f;
@@ -13,10 +22,15 @@ public class WorldGeneration : MonoBehaviour
     public Texture2D noiseTexture;
     public bool isSolid = true;
 
+    // For biomes
+    public Texture2D biomeMap;
+
+
     private void Start()
     {
         seed = Random.Range(-10000, 10000);
         GenerateNoiseTexture();
+        CreateChunks();
         GenerateTerrain();
     }
     public void GenerateNoiseTexture()
@@ -42,21 +56,58 @@ public class WorldGeneration : MonoBehaviour
 
             for (int y = 0; y < height; y++)
             {
-                // If statement for caves
-                if (noiseTexture.GetPixel(x,y).r > 0.1f) // Lower this value the lower the chance of caves being generated
+                //This simply determines which tile is used based on its position
+                Sprite tileSprite = stone;
+                
+                if (y < height - sandLayerHeight)
                 {
-                    GameObject newTile = new GameObject(name = "tile");
-                    newTile.transform.parent = this.transform;
-                    newTile.AddComponent<SpriteRenderer>();
-                    
-                    // So player doesnt fall through the floor
-                    newTile.AddComponent<BoxCollider2D>();
-                    newTile.GetComponent<BoxCollider2D>().size = Vector2.one;
-
-                    newTile.GetComponent<SpriteRenderer>().sprite = tile;
-                    newTile.transform.position = new Vector2(x + 0.5f, y + 0.5f);
+                    tileSprite = stone;
+                }
+                else if(y > height - 1)
+                {
+                    tileSprite = topSand;
+                }
+                else
+                {
+                    tileSprite = bottomSand;
+                }
+                // If statement for caves
+                if (noiseTexture.GetPixel(x, y).r > 0.1f) // Lower this value the lower the chance of caves being generated
+                {
+                    PlaceTiles(tileSprite, x, y);
                 }
             }
         }
+    }
+    public void CreateChunks()
+    {
+        // Using an array it groups up tiles based on the parameter "chunksize"
+        int numChunks = worldSize / chunkSize;
+        worldChunks = new GameObject[numChunks];
+        for (int i = 0; i < numChunks; i++)
+        {
+            GameObject newChunk = new GameObject();
+            newChunk.name = $"Chunk {i.ToString()}";
+            newChunk.transform.parent = this.transform;
+            worldChunks[i] = newChunk;
+        }
+    }
+    public void PlaceTiles(Sprite tileSprite, int x, int y)
+    {
+        GameObject newTile = new GameObject();
+
+        int chunkCoord = Mathf.RoundToInt(Mathf.Round(x / chunkSize) * chunkSize);
+        chunkCoord /= chunkSize;
+        newTile.transform.parent = worldChunks[chunkCoord].transform;
+
+        // So player doesnt fall through the floor
+        newTile.AddComponent<BoxCollider2D>();
+        newTile.GetComponent<BoxCollider2D>().size = Vector2.one;
+
+        // Adds all tiles to game scene
+        newTile.AddComponent<SpriteRenderer>();
+        newTile.GetComponent<SpriteRenderer>().sprite = tileSprite;
+        newTile.name = tileSprite.name;
+        newTile.transform.position = new Vector2(x + 0.5f, y + 0.5f);
     }
 }
