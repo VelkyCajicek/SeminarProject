46179833df5a5a35a83d23650e41b51c1b2c230b
krystof fric <krystoffric@krystofs-MacBook-Air.local>
2024-06-06 21:47:19 +0200

    parallax

--- Assets/Scripts/VFX/Parallax.cs
+++ Assets/Scripts/VFX/Parallax.cs
@@ -4,26 +4,33 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    public GameObject mainCamera;
-    float spriteLength;
-    float startPos;
-    public float parallaxSpeed=0.1f;
-    // Start is called before the first frame update
-    void Start()
+    public Camera cam;
+    public float parallaxStrengthX = 0.5f;
+    public float parallaxStrengthY = 0.5f;
+    private Transform camTransform;
+    private Vector3 lastCamPos;
+    private float spriteWidth;
+    private float spriteScaleX;
+
+    private void Start()
     {
-        spriteLength = GetComponent<SpriteRenderer>().bounds.size.x;
-        startPos = transform.position.x;
+        spriteScaleX = transform.localScale.x;
+        camTransform = cam.transform;
+        lastCamPos = camTransform.position;
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        Texture2D texture = sprite.texture;
+        spriteWidth = texture.width / sprite.pixelsPerUnit; 
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        //float temp = mainCamera.transform.position.x * (1 - parallaxSpeed);
-        float dist = mainCamera.transform.position.x * parallaxSpeed;
-        transform.position = new Vector3(startPos+dist,transform.position.y, transform.position.z);
-        if (Mathf.Abs(Mathf.Abs(startPos) - Mathf.Abs(transform.position.x))>spriteLength)
+        Vector3 distance = camTransform.position - lastCamPos;
+        transform.position += new Vector3(distance.x * parallaxStrengthX, distance.y * parallaxStrengthY);
+        lastCamPos = camTransform.position;
+        if (Mathf.Abs(camTransform.position.x - transform.position.x)>=spriteWidth * spriteScaleX)
         {
-            startPos = mainCamera.transform.position.x;
+            float offset = (camTransform.position.x - transform.position.x) % (spriteWidth * spriteScaleX);
+            transform.position = new Vector3(camTransform.position.x + offset, transform.position.y);
         }
     }
 }
